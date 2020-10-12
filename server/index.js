0694d714850265b710c00170526d1ba915581c38
JessicaWildermuth <jwildermuth536@gmail.com>
2020-10-12 15:50:11 -0700

    Add static images

--- server/index.js
+++ server/index.js
@@ -1,11 +1,48 @@
 const express = require('express');
+const path = require('path');
 
 const proxy = express();
+const port = 5000;
 
-const port = 3000;
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 proxy.use(express.static('public'));
-proxy.use(express.static('client'))
+proxy.use('/:id', express.static(path.join(__dirname, 'public')));
+
+proxy.use('/api/affordability',
+  createProxyMiddleware({
+    target: 'http://3.88.85.5:3003',
+    changeOrigin: true,
+  })
+);
+
+proxy.use('/api/affordability/:id',
+  createProxyMiddleware({
+    target: 'http://3.88.85.5:3003',
+    changeOrigin: true,
+  })
+);
+proxy.use('/api/similarhomes',
+  createProxyMiddleware({
+    target: 'http://18.216.126.142:80',
+    changeOrigin: true,
+  })
+);
+
+proxy.use('/api/addresses',
+  createProxyMiddleware({
+    target: 'http://54.176.5.242/',
+    changeOrigin: true,
+  })
+);
+
+// proxy.use('/api/addresses/:id=7',
+//   createProxyMiddleware({
+//     target: 'http://54.176.5.242/',
+//     changeOrigin: true,
+//   })
+// );
+
 
 proxy.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
