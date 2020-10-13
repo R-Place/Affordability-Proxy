81e3f716ec6c93599854dc32944d791df9c7d8ed
JessicaWildermuth <jwildermuth536@gmail.com>
2020-10-13 09:56:17 -0700

    Add css to organize components

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const proxy = express();
-const port = 5000;
+const port = 3000;
 
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
